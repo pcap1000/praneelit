24266369827d5c241ee981ff216e99464b0a67fc
pcap <praneelchettys@gmail.com>
2025-02-09 12:00:20 +0530

    change three

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,18 +1,23 @@
-import styles from '../styles/Logo.module.css';
+import { Link } from "react-router-dom";
+import styles from "../styles/Logo.module.css";
 
 function Logo() {
   return (
     <div className={styles.logoContainer}>
-      <img
-        className={styles.logo}
-        src="./img/reddit-logo-transparent.webp"
-        //  src="./public/img/reddit-logo-nobg.png"
-      />
-      <img
-        className={styles.name}
-        src="./img/praneelitorg.png"
-        //  src="./public/img/reddit-logo-nobg.png"
-      />
+      <Link to="/">
+        <img
+          className={styles.logo}
+          src="./img/reddit-logo-transparent.webp"
+          alt="Reddit Logo"
+        />
+      </Link>
+      <Link to="/">
+        <img
+          className={styles.name}
+          src="./img/praneelitorg.png"
+          alt="Praneelit Logo"
+        />
+      </Link>
     </div>
   );
 }
