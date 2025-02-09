fed2e3dd28b3e976bb5de0d71d3a16ce01a5c6cb
pcap <praneelchettys@gmail.com>
2025-02-09 16:09:54 +0530

    Change Number 7

--- src/ui/ProfileHeader.jsx
+++ src/ui/ProfileHeader.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/ProfileHeader.module.css';
-import CommunityProfile from './CommunityProfile';
+
 const ProfileHeader = () => {
+  const [activeTooltip, setActiveTooltip] = useState(null);
+
+  const handleClick = (button) => {
+    setActiveTooltip(activeTooltip === button ? null : button);
+  };
+
   return (
     <div className={styles.body}>
       <div className={styles.container}>
         <div className={styles.header}>
           <div className={styles.banner}>
             <img 
-              src="https://styles.redditmedia.com/t5_2qmjl/styles/bannerBackgroundImage_2qok6gpoiud71.png?width=2176&frame=1&auto=webp&s=383b34314d2cf814fb3f331fa0856128c673fb35" 
-              // src="https://preview.redd.it/new-batch-7680x2160-ultrawide-wallpapers-02-02-link-in-v0-y71483bumtge1.jpg?width=7680&format=pjpg&auto=webp&s=35c5cd02957313d13a3a40be1cb49e6f40fc55d5" 
+              src="https://styles.redditmedia.com/t5_2qmjl/styles/bannerBackgroundImage_2qok6gpoiud71.png?width=2176&frame=1&auto=webp&s=383b34314d2cf814fb3f331fa0856128c673fb35"  
               alt="Profile Banner" 
               className={styles.bannerImg}
             />
@@ -17,7 +22,7 @@ const ProfileHeader = () => {
           <div className={styles.profileSection}>
             <div className={styles.profilePicture}>
               <img 
-                src="./img/praneelpic.jpeg" 
+                src="src/images/praneelpic.jpeg" 
                 alt="Profile Picture" 
                 className={styles.profilePictureImg}
               />
@@ -25,23 +30,36 @@ const ProfileHeader = () => {
             <div className={styles.profileName}>r/praneelchetty</div>
           </div>
           <div className={styles.filters}>
-            <button className={styles.filterButton}>
+            <button 
+              className={styles.filterButton} 
+              data-tooltip="Fake" 
+              onClick={() => handleClick('top')}
+            >
               Top
               <span className={styles.filterButtonAfter}></span>
+              {activeTooltip === 'top' && <div className={styles.tooltip}>Fake</div>}
             </button>
-            <button className={styles.filterButton}>
+            <button 
+              className={styles.filterButton} 
+              data-tooltip="Fake" 
+              onClick={() => handleClick('allTime')}
+            >
               All Time
               <span className={styles.filterButtonAfter}></span>
+              {activeTooltip === 'allTime' && <div className={styles.tooltip}>Fake</div>}
             </button>
-            <button className={`${styles.filterButton} ${styles.menuButton}`}>
+            <button 
+              className={`${styles.filterButton} ${styles.menuButton}`} 
+              data-tooltip="Fake" 
+              onClick={() => handleClick('menu')}
+            >
               ⋮
+              {activeTooltip === 'menu' && <div className={styles.tooltip}>Fake</div>}
             </button>
           </div>
         </div>
       </div>
-      {/* <CommunityProfile/> */}
     </div>
-    
   );
 };
 
